d7872315722e975dac26719274ec172eb50d8ee0
saadi <34628341+hocinesaadi@users.noreply.github.com>
2022-02-26 21:57:18 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Survey Results')
+st.header('Survey Results 2021')
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+### --- LOAD DATAFRAME
+excel_file = 'Results.xlsx'
+sheet_name = 'DATA'
+
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
+
+df_participants = pd.read_excel(excel_file,
+                                sheet_name= sheet_name,
+                                usecols='F:G',
+                                header=3)
+df_participants.dropna(inplace=True)
+
+# --- STREAMLIT SELECTION
+department = df['Department'].unique().tolist()
+ages = df['Age'].unique().tolist()
+
+age_selection = st.slider('Age:',
+                        min_value= min(ages),
+                        max_value= max(ages),
+                        value=(min(ages),max(ages)))
+
+department_selection = st.multiselect('Department:',
+                                    department,
+                                    default=department)
+
+# --- FILTER DATAFRAME BASED ON SELECTION
+mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
+number_of_result = df[mask].shape[0]
+st.markdown(f'*Available Results: {number_of_result}*')
+
+# --- GROUP DATAFRAME AFTER SELECTION
+df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
+df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
+df_grouped = df_grouped.reset_index()
+
+# --- PLOT BAR CHART
+bar_chart = px.bar(df_grouped,
+                   x='Rating',
+                   y='Votes',
+                   text='Votes',
+                   color_discrete_sequence = ['#F50532']*len(df_grouped),
+                   template= 'seaborn')
+
+st.plotly_chart(bar_chart)
+
+# --- DISPLAY IMAGE & DATAFRAME
+
+# --- PLOT PIE CHART
+pie_chart = px.pie(df_participants,
+                title='Total No. of Participants',
+                values='Participants',
+                names='Departments')
